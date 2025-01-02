9c88e3c13e314721b26e6f778914437db50bc124
João de Vasconcelos <contact@joao.earth>
2025-01-02 10:17:48 +0000

    Remove snow

--- screens/AllScreens/index.tsx
+++ screens/AllScreens/index.tsx
@@ -7,7 +7,7 @@ import { Screen1 } from '@/screens/Screen1';
 import { Screen2 } from '@/screens/Screen2';
 import { Screen3 } from '@/screens/Screen3';
 import { Screen4 } from '@/screens/Screen4';
-import Snowfall from 'react-snowfall';
+// import Snowfall from 'react-snowfall';
 
 import styles from './styles.module.css';
 
@@ -21,7 +21,7 @@ export function AllScreens() {
 			<Screen3 />
 			<Screen4 />
 			<Compliance />
-			<Snowfall snowflakeCount={40} speed={[0.5, 1]} wind={[0, 1]} />
+			{/* <Snowfall snowflakeCount={40} speed={[0.5, 1]} wind={[0, 1]} /> */}
 		</div>
 	);
 }
