14e82064fcac3be01b9589aa4b2cd4ab52b52b6b
Classic-German <175220123+Classic-German@users.noreply.github.com>
2025-06-30 17:45:24 +0100

    Create Area1 folder and all Area1 working

---
+++ app/Area1/screens/AllScreens/index.tsx
@@ -0,0 +1,30 @@
+'use client';
+
+/* * */
+
+import { Compliance } from '@/components/Compliance';
+
+import { Screen1 } from '../Screen1/index';
+import { Screen2 } from '../Screen2/index';
+import { Screen3 } from '../Screen3/index';
+import { Screen4 } from '../Screen4/index';
+// import Confetti from 'react-confetti';
+// import Snowfall from 'react-snowfall';
+
+import styles from './styles.module.css';
+
+/* * */
+
+export function AllScreens() {
+	return (
+		<div className={styles.container}>
+			<Screen1 />
+			<Screen2 />
+			<Screen3 />
+			<Screen4 />
+			<Compliance />
+			{/* <Confetti height={1080} numberOfPieces={50} width={1920} /> */}
+			{/* <Snowfall snowflakeCount={40} speed={[0.5, 1]} wind={[0, 1]} /> */}
+		</div>
+	);
+}
---
+++ app/Area1/screens/Screen1/index.tsx
@@ -0,0 +1,75 @@
+'use client';
+
+/* * */
+
+import { CardDefault } from '@/components/CardDefault';
+import { Grid } from '@/components/Grid';
+import { IconCreditCardPay } from '@tabler/icons-react';
+import { useMemo } from 'react';
+import useSWR from 'swr';
+
+/* * */
+
+export function Screen1() {
+	//
+
+	//
+	// A. Fetch data
+
+	const { data: validationsData, isLoading: validationsLoading, isValidating: validationsValidating } = useSWR('https://api.carrismetropolitana.pt/v2/metrics/videowall/validations');
+
+	//
+	// B. Transform data
+
+	const validationsCmParsed = useMemo(() => {
+		if (!validationsData) return { primary_value: 0, secondary_value: 0, secondary_value_string: '?%' };
+		return {
+			primary_value: validationsData.data._cm_today_valid_count,
+			primary_value_string: Intl.NumberFormat('pt-PT').format(validationsData.data._cm_today_valid_count),
+			secondary_value: validationsData.data._cm_today_valid_count / validationsData.data._cm_last_week_valid_count,
+			secondary_value_string: `${parseFloat(((validationsData.data._cm_today_valid_count * 100) / validationsData.data._cm_last_week_valid_count).toFixed(2))}%`,
+		};
+	}, [validationsData]);
+
+	const validations41Parsed = useMemo(() => {
+		if (!validationsData) return { primary_value: 0, secondary_value: 0, secondary_value_string: '?%' };
+		return {
+			primary_value: validationsData.data._41_today_valid_count,
+			primary_value_string: Intl.NumberFormat('pt-PT').format(validationsData.data._41_today_valid_count),
+			secondary_value: validationsData.data._41_today_valid_count / validationsData.data._41_last_week_valid_count,
+			secondary_value_string: `${parseFloat(((validationsData.data._41_today_valid_count * 100) / validationsData.data._41_last_week_valid_count).toFixed(2))}%`,
+		};
+	}, [validationsData]);
+	//
+	// C. Render components
+
+	return (
+		<Grid
+			layout="primaryWithFourDetails"
+			cells={[
+				<CardDefault
+					icon={<IconCreditCardPay />}
+					isLoading={validationsLoading}
+					isValidating={validationsValidating}
+					sentiment={validationsCmParsed.secondary_value < 1 ? 'normal' : 'good'}
+					timestamp={validationsData?.timestamp_resource}
+					title="CM / Passageiros transportados hoje, até agora"
+					valuePrimary={validationsCmParsed.primary_value_string}
+					valueSecondary={validationsCmParsed.secondary_value_string}
+				/>,
+				<CardDefault
+					icon={<IconCreditCardPay />}
+					isLoading={validationsLoading}
+					isValidating={validationsValidating}
+					sentiment={validations41Parsed.secondary_value < 1 ? 'normal' : 'good'}
+					timestamp={validationsData?.timestamp_resource}
+					title="41 / Passageiros transportados hoje, até agora"
+					valuePrimary={validations41Parsed.primary_value_string}
+					valueSecondary={validations41Parsed.secondary_value_string}
+				/>,
+			]}
+		/>
+	);
+
+	//
+}
