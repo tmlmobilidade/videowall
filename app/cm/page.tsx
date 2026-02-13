2ddaa0ff259b2afab172c78751df30572a1a0ee4
João de Vasconcelos <johnyvasconcelos@icloud.com>
2026-02-13 11:06:31 +0000

    Remove password

--- app/cm/page.tsx
+++ app/cm/page.tsx
@@ -1,14 +1,9 @@
 /* * */
 
 import { AllScreens } from '@/areas/cm/AllScreens';
-import { PasswordCheck } from '@/components/PasswordCheck';
 
 /* * */
 
 export default function Page() {
-	return (
-		<PasswordCheck id="cm" password="cmet">
-			<AllScreens />
-		</PasswordCheck>
-	);
+	return <AllScreens />;
 }
